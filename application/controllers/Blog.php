3a51d7d5b88daf6f14fc8b47d21917deb3e232ba
dengliang.wang <dengliang.wang@fictiongame.com>
2017-09-28 15:21:53 +0800

    modify blog

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -15,14 +15,14 @@ class Blog  extends CI_Controller{
     
     public function __construct() {
         parent::__construct();
-        $this->load->databases();
+        $this->load->database();
     }
     //put your code here
     public function index()
     {
         //echo 'hello word';
-        $this->load->view('blogview');
-        $game = $this->db->query('SELECT * FROM codeIg_test_wdl.news;');
+        //$this->load->view('blogview');
+        $game = $this->db->query('SELECT * FROM news;');
         
         foreach ($game->result_array() as $g) {
             echo $g->title; 
